bde12115569d42b2b111320d0a0ae3f0128fe5a5
Chris Murphy <chris@strandz.org>
2016-01-18 04:07:06 +1100

    Semantic UI working

--- project.clj
+++ project.clj
@@ -7,9 +7,6 @@
                  [cljsjs/react-dom "0.14.3-1"]
                  [devcards "0.2.1"]
                  [org.clojure/test.check "0.9.0"]
-                 ;[bootstrap-cljs "0.0.3" :exclusions [org.clojure/clojure]]
-                 [cljsjs/react-bootstrap "0.27.3-0"]
-                 ;[sablono "0.5.3"]
                  ]
 
   :plugins [[lein-cljsbuild "1.1.2"]
--- src/om_alarming/components/nav.cljs
+++ src/om_alarming/components/nav.cljs
@@ -2,31 +2,24 @@
   (:require [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
             [om-alarming.util :refer [class-names index-of]]
-            [cljsjs.react-bootstrap]
             ))
 
-(defn button [info]
-  ;(js/ReactBootstrap.Nav (js->clj {:key (:id info)}) (:name info))
-  (dom/li (js->clj {:key (:id info)}) (:name info))
-  )
+(defui Button
+  Object
+  (render [this]
+    (let [info (om/props this)
+          _ (println info)]
+      (dom/a #js {:key (:id info) :className "item"} (:name info)))))
+
+(def button (om/factory Button {:keyfn :id}))
 
 (defui MenuBar
   Object
   (render [this]
     (let [items (:buttons (om/props this))
-          ;_ (println "Got " (count items))
-          ]
-
-      (js/ReactBootstrap.Navbar (js->clj {:brand (dom/a {:href "#"} "Navbar")}))
-
-      (js/ReactBootstrap.NavItem (js->clj {:bs-style   "pills"
-                        :active-key 1
-                        :on-select  (fn [k _] (js/alert (str "Selected " k)))})
+          _ (println "Got " (count items))]
+      (dom/div #js {:className "ui pointing menu"}
               (for [item items]
-                (button item)))
+                (button item))))))
 
-      ;(button {:id "A" :name "B"})
-
-      )))
-
-(def menubar (om/factory MenuBar))
+(def menubar (om/factory MenuBar {:keyfn :id}))
