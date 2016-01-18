941a753d1cb96e0241b262885829590f1d5c7e04
Chris Murphy <chris@strandz.org>
2016-01-19 01:09:40 +1100

    Got row headings. Next will be first column

--- src/om_alarming/components/grid.cljs
+++ src/om_alarming/components/grid.cljs
@@ -8,16 +8,13 @@
   Object
   (render [this]
     (let [props (om/props this)
-          computed (om/get-computed this)
+          ;computed (om/get-computed this)
           test-props (:test-props props)
           selected (or (:selected props) (:selected test-props))
-          full-name (or (:full-name computed) (:full-name test-props))
-          _ (println full-name "is selected:" selected)
+          ;full-name (or (:full-name computed) (:full-name test-props))
+          ;_ (println full-name "is selected:" selected)
           ]
-      (dom/div #js {:className
-                    (str "ui" (if selected " checked " " ") "checkbox")
-                    ;"ui checked checkbox"
-                    }
+      (dom/div #js {:className (str "ui" (if selected " checked " " ") "checkbox")}
                (dom/input #js {:type "checkbox" :checked (when selected " ")})
                (dom/label nil "")))))
 
@@ -29,19 +26,19 @@
 ;; <label>Make my profile visible</label>
 ;; </div>
 ;;
-(defui GridCell
+(defui GridDataCell
   Object
   (render [this]
     (let [{:keys [id gas selected] :as props} (om/props this)
           gas-name (-> bus/gas->details gas :name)
           {:keys [tube-num]} (om/get-computed this)
           full-name (str "Tube " tube-num " " gas-name)
-          _ (println full-name ", selected" selected)
+          ;_ (println full-name ", selected" selected)
           ]
-      (dom/div #js {:className "two wide column center aligned"}
+      (dom/div #js {:className "three wide column center aligned"}
                (checkbox (om/computed props {:full-name full-name}))))))
 
-(def grid-cell (om/factory GridCell {:keyfn :id}))
+(def grid-data-cell (om/factory GridDataCell {:keyfn :id}))
 
 (defui GridRow
   Object
@@ -49,15 +46,31 @@
     (let [{:keys [id gases]} (om/props this)]
       (dom/div #js {:className "row"}
                (for [gas gases]
-                 (grid-cell (om/computed gas {:tube-num id})))))))
+                 (grid-data-cell (om/computed gas {:tube-num id})))))))
 
+(defui GridHeaderRow
+  Object
+  (render [this]
+    (let [{:keys [id gases]} (om/props this)]
+      (dom/div #js {:className "row"}
+               (for [gas gases
+                     :let [gas-kw (:gas gas)
+                           _ (println "GAS:" gas-kw)
+                           gas-name (-> bus/gas->details gas-kw :name)
+                           _ (println "gas:" gas-name)]]
+                 (dom/div #js {:className "three wide column center aligned"}
+                          (dom/label nil gas-name)))))))
+
+(def grid-header-row (om/factory GridHeaderRow {:keyfn :id}))
 (def grid-row (om/factory GridRow {:keyfn :id}))
 
 (defui GasSelectionGrid
   Object
   (render [this]
-    (let [tubes (:tubes (om/props this))]
+    (let [tubes (:tubes (om/props this))
+          gases (:gases (nth tubes 0))]
       (dom/div #js {:className "ui five column grid"}
+               (grid-header-row {:gases gases})
                (for [tube tubes]
                  (grid-row tube))))))
 
