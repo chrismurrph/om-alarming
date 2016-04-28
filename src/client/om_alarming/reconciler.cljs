c2c54f187d6ce15d8a794b9e3ba97ee012a16740
Chris Murphy <chris@strandz.org>
2016-04-28 13:48:39 +1000

    Won't compile, just retrieving the old methods

--- src/client/om_alarming/reconciler.cljs
+++ src/client/om_alarming/reconciler.cljs
@@ -8,3 +8,26 @@
 
 (defn internal-query [one]
   (println "internal-query" one))
+
+;; (in-ns 'om-alarming.reconciler)
+(defn top-level-query
+  "Only use for top level keywords. Returns the actual value rather than a hashmap"
+  [kw]
+  ;(println "External query for " kw)
+  (let [res (my-parser {:state my-reconciler} `[[~kw _]])]
+    (when (not-empty res) (apply val res))))
+
+(defn internal-query
+  "Just invokes the read. Caller to deal with the hashmap that is returned??"
+  [query]
+  ;(println "External query for " kw)
+  (let [res (my-parser {:state my-reconciler} query)]
+    res))
+
+;; (alteration 'app/tab {:new-id 1} :app/selected-button)
+(defn alteration
+  ([mutate-key param-map kw]
+   (om/transact! my-reconciler `[(~mutate-key ~param-map) ~kw]))
+  ([mutate-key param-map]
+   (om/transact! my-reconciler `[(~mutate-key ~param-map)]))
+  )
